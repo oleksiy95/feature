b792fbd2b267246e7be32e699812d92eafee47c9
Oleksii Oleksiv <bodyart3003@gmail.com>
2021-09-24 18:37:20 +0300

    write github component test

--- src/app/shared/services/github.service.spec.ts
+++ src/app/shared/services/github.service.spec.ts
@@ -1,13 +1,19 @@
 import { TestBed } from '@angular/core/testing';
+import { ApiService } from './api.service';
 
 import { GithubService } from './github.service';
 
 describe('GithubService', () => {
   let service: GithubService;
+  let apiServiceSpy: jasmine.SpyObj<ApiService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const spy = jasmine.createSpyObj('ApiService', ['get', 'post', 'put', 'delete']);
+    TestBed.configureTestingModule({
+      providers: [{provide: ApiService, useValue: spy}]
+    });
     service = TestBed.inject(GithubService);
+    apiServiceSpy = TestBed.inject(ApiService) as jasmine.SpyObj<ApiService>;
   });
 
   it('should be created', () => {
